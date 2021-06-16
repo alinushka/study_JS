511825667bec13cf6001ab8128936948daf42949
alin.ushka <salatbulka@mail.ru>
2021-06-16 20:05:12 +0400

    сделана легкая часть

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 
 let money = 5500;
 let income = 'фриланс';
-let addExpenses = 'интернет, такси, коммуналка';
+let addExpenses = 'Интернет, Такси, Коммуналка';
 let deposit = true;
 let mission = 150000;
 let period = 6;
@@ -15,8 +15,8 @@ console.log(addExpenses.length);
 
 console.log('\"Период равен ' + period + ' месяцев\"' + ' и ' + '\"Цель заработать ' + mission + ' рублей\"');
 
-
-
+let massiv = addExpenses.toLowerCase();
+console.log(massiv.split(', '));
 
 let budgetDay = money / 30;
 console.log('budgetDay: ', budgetDay);
