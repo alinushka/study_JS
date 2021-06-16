1b91980d6f882daade96e950049aa21f14750817
alin.ushka <salatbulka@mail.ru>
2021-06-16 19:53:07 +0400

    почти сделала 2 урок

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,22 @@
-"use strict";
+'use strict';
 
-let myVar;
+let money = 5500;
+let income = 'фриланс';
+let addExpenses = 'интернет, такси, коммуналка';
+let deposit = true;
+let mission = 150000;
+let period = 6;
 
-myVar = true;
-console.log(typeof true);
+console.log('money =', typeof money);
+console.log('income =', typeof income);
+console.log('deposit =', typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('\"Период равен ' + period + ' месяцев\"' + ' и ' + '\"Цель заработать ' + mission + ' рублей\"');
+
+
+
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
